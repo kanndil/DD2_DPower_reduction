4f9db0ed362cfd8f05d9a4051278833eca050e6c
Youssef Kandil <63082375+youssefkandil@users.noreply.github.com>
2021-12-04 15:32:17 +0200

    manual testing with yosys to figure out the GLNL

--- testing_yosys/test1.v
+++ testing_yosys/test1.v
@@ -1,26 +1,11 @@
-
-
 module test1
 (
-  input [3:0] x,
-  input [3:0] y,
+  input x,
   input s,
   input clk,
-  output [3:0] r
+  output reg r
 );
-
-  wire new_clk;
-  reg new_enable;
-  reg [3:0] r;
-  assign new_clk = (new_enable & clk);
-
-  always @(clk) begin
-    new_enable <= s;
-  end
-
-
-  always @(posedge new_clk) r <= x + y;
+  always @(posedge clk) r <= x ;
 
 
 endmodule
-
