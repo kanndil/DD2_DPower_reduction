3aaf8afba0f89c356400f25887bb36425c3fc405
Youssef Kandil <63082375+youssefkandil@users.noreply.github.com>
2021-12-04 16:57:28 +0200

    completing ICG code

--- Code/Dynamic_power_reducer.py
+++ Code/Dynamic_power_reducer.py
@@ -21,38 +21,55 @@ newrtl= []
 new_clk = vast.Wire('new_clk')
 new_enable= vast.Reg('new_enable')
 newrtl.append(new_clk)
-
 newrtl.append(new_enable)
 
 ####################################################
 
+port_list=definition.portlist.ports
+
+
 for itemDeclaration in definition.items:
     item_type = type(itemDeclaration).__name__
     if item_type == "Always":
-        #print('x')
-
-        assign_gate_clk = vast.NonblockingSubstitution(
-        vast.Lvalue(vast.Identifier('new_enable')),
-        vast.Rvalue(itemDeclaration.statement.cond))
-        statement_latch = vast.Block([ assign_gate_clk ])
-        sens_latch = vast.Sens(itemDeclaration.sens_list.list[0].sig, type='') 
-        senslist_latch = vast.SensList([ sens_latch ])
-        always_latch = vast.Always(senslist_latch, statement_latch)
+        for port in port_list:
 
 
-        right_assign=vast.And(vast.Identifier('new_enable'),itemDeclaration.sens_list.list[0].sig )
-        
-        assign_nw_clk= vast.Assign(vast.Lvalue(vast.Identifier('new_clk')), right_assign)
+                if(str(port.first.name) == str(itemDeclaration.statement.cond)):
+                    print("in")
+                    #designing latch statement
+                    
+                    if0_true = vast.NonblockingSubstitution(
+                    vast.Lvalue(vast.Identifier('new_enable')),
+                    vast.Rvalue(itemDeclaration.statement.cond))
+
+                    if0_false = vast.NonblockingSubstitution(
+                    vast.Lvalue(vast.Identifier('new_enable')),
+                    vast.Rvalue(vast.Identifier('new_enable')))
+                    if0 = vast.IfStatement(vast.Identifier('~'+str(itemDeclaration.sens_list.list[0].sig)), if0_true, if0_false)
+                    statement_latch = vast.Block([ if0 ])
+                    
+
+                    #designing latch senslist
+                    sens_latch_clk = vast.Sens(itemDeclaration.sens_list.list[0].sig, type='') 
+                    sens_latch_en= vast.Sens(itemDeclaration.statement.cond, type='') ####
+                    senslist_latch = vast.SensList([ sens_latch_clk, sens_latch_en])
+
+                    always_latch = vast.Always(senslist_latch, statement_latch)
+                   
+
+                    right_assign=vast.And(vast.Identifier('new_enable'),itemDeclaration.sens_list.list[0].sig )
+                    
+                    assign_nw_clk= vast.Assign(vast.Lvalue(vast.Identifier('new_clk')), right_assign)
 
 
 
-        sens_alw = vast.Sens(vast.Identifier('new_clk'), type='posedge')
-        senslist_alw = vast.SensList([ sens_alw ])
-        statement=itemDeclaration.statement.true_statement
-        always = vast.Always(senslist_alw, statement)
-        newrtl.append(assign_nw_clk)
-        newrtl.append(always_latch)
-        newrtl.append(always)
+                    sens_alw = vast.Sens(vast.Identifier('new_clk'), type='posedge')
+                    senslist_alw = vast.SensList([ sens_alw ])
+                    statement=itemDeclaration.statement.true_statement
+                    always = vast.Always(senslist_alw, statement)
+                    newrtl.append(assign_nw_clk)
+                    newrtl.append(always_latch)
+                    newrtl.append(always)
     else:
 
         newrtl.append(itemDeclaration)
